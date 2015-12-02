3be63cd060976cd43120b0a43cceb84aec85c378
arit_nath <arit.nath@dcplkolkata.com>
2015-12-02 20:06:27 +0530

    yml form the platform

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Prepare the settings file for installation
-if [ ! -f /app/public/web/sites/default/settings.php ]
+if [ ! -f web/sites/default/settings.php ]
   then
-    cp /app/public/web/sites/default/default.settings.php /app/public/web/sites/default/settings.php
-    chmod 777 /app/public/web/sites/default/settings.php
+    cp web/sites/default/default.settings.php web/sites/default/settings.php
+    chmod 777 web/sites/default/settings.php
 fi
 
 # Prepare the services file for installation
-if [ ! -f /app/public/web/sites/default/services.yml ]
+if [ ! -f web/sites/default/services.yml ]
   then
-    cp /app/public/web/sites/default/default.services.yml /app/public/web/sites/default/services.yml
-    chmod 777 /app/public/web/sites/default/services.yml
+    cp web/sites/default/default.services.yml web/sites/default/services.yml
+    chmod 777 web/sites/default/services.yml
 fi
 
 # Prepare the files directory for installation
-if [ ! -d /app/public/web/sites/default/files ]
+if [ ! -d web/sites/default/files ]
   then
-    mkdir -m777 /app/public/web/sites/default/files
+    mkdir -m777 web/sites/default/files
 fi
