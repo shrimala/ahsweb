81fc25895f90fc366c019d68685cb6a1cd817984
arit_nath <arit.nath@dcplkolkata.com>
2015-12-04 12:46:18 +0530

    Adding sample composer.json file

--- settings.php
+++ settings.php
@@ -24,17 +24,32 @@ $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 // Override paths for config files in Platform.sh.
+// Define a config sync directory outside the document root.
 if (isset($_ENV['PLATFORM_APP_DIR'])) {
-  $config_directories = array(
-    CONFIG_SYNC_DIRECTORY => $_ENV['PLATFORM_APP_DIR'] . '/config/sync',
-  );
+  $config_directories[CONFIG_SYNC_DIRECTORY] = $_ENV['PLATFORM_APP_DIR'] . '/config/sync';
 }
 
-// Configure relationships.
-if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
+// Set trusted hosts based on real Platform.sh routes.
+if (isset($_ENV['PLATFORM_ROUTES'])) {
+  $routes = json_decode(base64_decode($_ENV['PLATFORM_ROUTES']), TRUE);
+  $settings['trusted_host_patterns'] = array();
+  foreach ($routes as $url => $route) {
+    $host = parse_url($url, PHP_URL_HOST);
+    if ($host !== FALSE && $route['type'] == 'upstream' && $route['upstream'] == $_ENV['PLATFORM_APPLICATION_NAME']) {
+      $settings['trusted_host_patterns'][] = '^' . preg_quote($host) . '$';
+    }
+  }
+  $settings['trusted_host_patterns'] = array_unique($settings['trusted_host_patterns']);
+}
+
+// Because we're using the Composer toolstack, we replicate the necessary local
+// settings file for Drupal here.
+
+if (getenv("PLATFORM_RELATIONSHIPS")) {
+  // Configure relationships.
   $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), TRUE);
 
-  if (empty($databases['default']['default']) && !empty($relationships['database'])) {
+  if (empty($databases['default']['default'])) {
     foreach ($relationships['database'] as $endpoint) {
       $database = array(
         'driver' => $endpoint['scheme'],
@@ -42,7 +57,6 @@ if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
         'username' => $endpoint['username'],
         'password' => $endpoint['password'],
         'host' => $endpoint['host'],
-        'port' => $endpoint['port'],
       );
 
       if (!empty($endpoint['query']['compression'])) {
@@ -57,29 +71,41 @@ if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
       }
     }
   }
-}
 
-// Import variables prefixed with 'drupal:' into $settings.
-if (isset($_ENV['PLATFORM_VARIABLES'])) {
+  $routes = json_decode(base64_decode($_ENV['PLATFORM_ROUTES']), TRUE);
+
+  if (!isset($conf['file_private_path'])) {
+    if(!$application_home = getenv('PLATFORM_APP_DIR')) {
+      $application_home = '/app';
+    }
+    $conf['file_private_path'] = $application_home . '/private';
+    $conf['file_temporary_path'] = $application_home . '/tmp';
+  }
+
   $variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), TRUE);
 
   $prefix_len = strlen('drupal:');
   foreach ($variables as $name => $value) {
     if (substr($name, 0, $prefix_len) == 'drupal:') {
-      $settings[substr($name, $prefix_len)] = $value;
+      $conf[substr($name, $prefix_len)] = $value;
     }
-  }	
+  }
+
+  // Default PHP settings.
+  ini_set('session.gc_probability', 1);
+  ini_set('session.gc_divisor', 100);
+  ini_set('session.gc_maxlifetime', 200000);
+  ini_set('session.cookie_lifetime', 2000000);
+  ini_set('pcre.backtrack_limit', 200000);
+  ini_set('pcre.recursion_limit', 200000);
 }
 
-// Default PHP settings.
-ini_set('session.gc_probability', 1);
-ini_set('session.gc_divisor', 100);
-ini_set('session.gc_maxlifetime', 200000);
-ini_set('session.cookie_lifetime', 2000000);
-ini_set('pcre.backtrack_limit', 200000);
-ini_set('pcre.recursion_limit', 200000);
+// Force Drupal not to check for HTTP connectivity until we fixed the self test.
+$conf['drupal_http_request_fails'] = FALSE;
 
-// Local settings.
+// Local settings. These allow local development environments to use their own
+// database connections rather than the Platform-only settings above.
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
+
