715a8ccd533b5bebbc4dfe2c65bc9a37fbb88603
Arit Kumar Nath <arit.nath@dcplkolkata.com>
2016-05-24 15:33:12 +0530

    location

--- settings.php
+++ settings.php
@@ -36,7 +36,7 @@ if (isset($_ENV['PLATFORM_APP_DIR'])) {
     //CONFIG_SYNC_DIRECTORY => $_ENV['PLATFORM_APP_DIR'] . '/config/staging',
   );
 }
-$config_directories[CONFIG_SYNC_DIRECTORY] = __DIR__ . 'files/config/sync';
+$config_directories[CONFIG_SYNC_DIRECTORY] = __DIR__ . '/files/config/sync';
 // Set trusted hosts based on real Platform.sh routes.
 if (isset($_ENV['PLATFORM_ROUTES'])) {
   $routes = json_decode(base64_decode($_ENV['PLATFORM_ROUTES']), TRUE);
