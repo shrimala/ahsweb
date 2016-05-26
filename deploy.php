a8fa8fbb6b30244a618781478b101dd0adc11e6f
Arit Kumar Nath <arit.nath@dcplkolkata.com>
2016-05-26 21:23:16 +0530

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,13 +1,15 @@
 <?php
 echo "Export all the configuration file to GitHub";
 echo shell_exec("drush -y config-export");
-//echo shell_exec("git clone -b ConfigExport2 https://username:d17b0dd947785dd6eb8b234e71bc0c8532de4fe3@github.com/shrimala/ahsweb.git");
+echo shell_exec("git clone -b ConfigExport2 https://username:e7fa092eaf3097c6ef4ac72c0ebb79fd40e4e721@github.com/shrimala/ahsweb.git");
 echo shell_exec("chmod -R 777 ahsweb");
-//echo shell_exec("cp /app/public/sites/default/files/config/sync/* /app/public/sites/default/files/ahsweb/config/sync/");
+echo shell_exec("cp /app/public/sites/default/files/config/sync/* /app/public/sites/default/files/ahsweb/config/sync/");
 echo shell_exec("cd ahsweb/config/sync");
 echo shell_exec("git add --all");
+echo shell_exec("Git config  user.email 'arith.nath@dcplkolkata.com'");
+echo shell_exec("git config  user.name 'aritnath1990'");
 echo shell_exec("git commit -am 'update message'");
-echo shell_exec("git push origin ConfigExport");
+echo shell_exec("git push origin ConfigExport2");
 /**echo shell_exec("cd ahsweb");
 echo shell_exec("git config user.email 'arith.nath@dcplkolkata.com'");
 echo shell_exec("git config user.name 'aritnath1990'");
