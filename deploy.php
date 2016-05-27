d95cea4730213104e092c9c28e80965b426aa4c0
Arit Kumar Nath <arit.nath@dcplkolkata.com>
2016-05-27 16:13:26 +0530

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
 echo "Export all the configuration file to GitHub";
 echo shell_exec("drush -y config-export");
-echo shell_exec("git clone -b ConfigExport2 https://username:c552571373e0f5525d3b208e968c1a8849b53a9b@github.com/shrimala/ahsweb.git");
+echo shell_exec("git clone -b ConfigExport2 https://username:asdfghjkl2222334555pppp1p2p1p212121njn23j232323@github.com/shrimala/ahsweb.git");
 echo shell_exec("chmod -R 777 ahsweb");
 echo shell_exec("cp /app/public/sites/default/files/config/sync/* /app/public/sites/default/files/ahsweb/config/sync/");
 echo shell_exec("cd /app/public/sites/default/files/ahsweb/config/sync");
