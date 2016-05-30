5b85168e7f358d326e288e20bbd8b9a60a512233
jonathanjfshaw <jonathanjfshaw@gmail.com>
2016-05-30 19:18:01 +0100

    Add missing semicolons

--- deploy.php
+++ deploy.php
@@ -21,9 +21,9 @@ runcmd("git clone -b ConfigExport2 https://{$GITHUB_TOKEN}@github.com/shrimala/a
 //runcmd("$ git pull origin ConfigExport2 https://{$GITHUB_TOKEN}@github.com/shrimala/ahsweb.git");
 runcmd("chmod -R 777 ahsweb");
 runcmd("cd /app/public/sites/default/files/ahsweb");
-runcmd("ls config/sync")
+runcmd("ls config/sync");
 runcmd("drush -y config-export");
-runcmd("ls config/sync")
+runcmd("ls config/sync");
 runcmd("chmod -R 777 /app/public/sites/default/files/ahsweb/config/sync");
 runcmd("git add config/sync");
 runcmd("git status");
