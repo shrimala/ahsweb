d65cb27b8dd7edc2f916c95e3ab4b8f846166149
jonathanjfshaw <jonathanjfshaw@gmail.com>
2016-06-01 19:29:51 +0100

    Dynamic branch name

--- deploy.php
+++ deploy.php
@@ -27,23 +27,28 @@ echo "<br><br><br><br><br><br>";
 echo "Export all the configuration file to GitHub<br>";
 $platform_variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), TRUE);
 $GITHUB_TOKEN = $platform_variables["GITHUB_TOKEN"];
-$ip=shell_exec("curl -s https://api.github.com/repos/shrimala/ahsweb/pulls/37");
+$environment = $platform_variables["PLATFORM_ENVIRONMENT"];
+echo "Environment name: " . $environment;
+$pr = substr($environment, 4, strpos($environment,":");
+echo "PR number: ". $pr;
+$ip=shell_exec("curl -s https://api.github.com/repos/shrimala/ahsweb/pulls/" . $pr);
 $json = json_decode($ip, true); 
 echo $json['head']['ref'];
-$dynBatch = $json['head']['ref'];
+$branch = $json['head']['ref'];
+echo "Branch name: " . $branch;
 runcmd("cd /app/public/sites/default/files;
 mkdir ahsweb;
 chmod -R 777 ahsweb/config/sync;
 cd ahsweb;
 git init;
-git pull https://{$GITHUB_TOKEN}@github.com/shrimala/ahsweb.git {$dynBatch};
-git checkout -b {$dynBatch};
+git pull https://{$GITHUB_TOKEN}@github.com/shrimala/ahsweb.git {$branch};
+git checkout -b {$branch};
 drush -y config-export;
 git add config/sync/;
 git config  user.email 'owner@ahs.org.uk';
 git config  user.name 'AHSowner';
 git commit -m '{$_POST['t1']}';
-git push https://{$GITHUB_TOKEN}@github.com/shrimala/ahsweb.git {$dynBatch}");
+git push https://{$GITHUB_TOKEN}@github.com/shrimala/ahsweb.git {$branch}");
 }
 ?>
 </body>
