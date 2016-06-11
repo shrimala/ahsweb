e0cd1b94ac7fc09c317bdb07f8ad621cfb5f9696
jonathanjfshaw <jonathanjfshaw@gmail.com>
2016-06-11 20:33:52 +0100

    Show seconds in ls in deploy.php

--- deploy.php
+++ deploy.php
@@ -44,20 +44,15 @@ $repo_path = $base_path . "/ahsweb";
 runcmd("rm -rf ahsweb", $base_path);
 runcmd("mkdir ahsweb", $base_path);
 runcmd("chmod -R 777 ahsweb", $base_path);
-runcmd("chmod -R 777 ahsweb", $base_path);
 runcmd("git init", $repo_path);
 runcmd("git pull https://{$GITHUB_TOKEN}@github.com/shrimala/ahsweb.git {$branch}", $repo_path);
 runcmd("git name-rev --name-only HEAD", $repo_path);
 runcmd("git checkout -b {$branch}", $repo_path);
 runcmd("git name-rev --name-only HEAD", $repo_path);
 runcmd("chmod -R 777 config/sync", $repo_path);
-//runcmd("rm config/sync/*", $repo_path);
-runcmd("ls -l config", $repo_path);
-runcmd("cat config/sync/system.site.yml", $repo_path);
-runcmd("cat config/sync/", $repo_path);
+runcmd("ls --full-time config", $repo_path);
 runcmd("drush -y config-export --destination=sites/default/files/ahsweb/config/sync;", $base_path);
-runcmd("ls -l config", $repo_path);
-runcmd("cat config/sync/system.site.yml", $repo_path);
+runcmd("ls --full-time config", $repo_path);
 runcmd("git config core.filemode false", $repo_path);
 runcmd("git add config/sync/", $repo_path);
 runcmd("git status;", $repo_path);
@@ -65,7 +60,7 @@ runcmd("git config  user.email 'owner@ahs.org.uk'", $repo_path);
 runcmd("git config  user.name 'AHSowner'", $repo_path);
 runcmd("git commit -m '{$_POST['t1']}'", $repo_path);
 runcmd("git push https://{$GITHUB_TOKEN}@github.com/shrimala/ahsweb.git {$branch}", $repo_path);
-
+runcmd("rm -rf ahsweb", $base_path);
 }
 ?>
 </body>
