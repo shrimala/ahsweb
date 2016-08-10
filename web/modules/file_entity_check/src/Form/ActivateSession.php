0b86660fc602287ad767e81ce96999aa250d69c0
arit_nath <arit.nath@dcplkolkata.com>
2016-08-10 15:49:35 +0530

    batch process60

--- web/modules/file_entity_check/src/Form/ActivateSession.php
+++ web/modules/file_entity_check/src/Form/ActivateSession.php
@@ -49,7 +49,7 @@ public function submitForm(array &$form, FormStateInterface $form_state) {
 			 
 		 }*/
 		 
-		 $q1 = db_query("SELECT fid,filename,uri  FROM file_managed where fid=1");// WHERE filemime='audio/mpeg' order by fid");
+		 $q1 = db_query("SELECT fid,filename,uri  FROM file_managed where fid<10");// WHERE filemime='audio/mpeg' order by fid");
          $result=array();
         foreach($q1 as $r)
          {
