abda6e9523f1b5cb77237157b648fc3c6931da19
jonathanjfshaw <jonathanjfshaw@gmail.com>
2016-08-25 17:14:44 +0100

    file_checker.batch_total should be $batches not $file_count

--- web/modules/file_checker/src/FilesCheckerManager.php
+++ web/modules/file_checker/src/FilesCheckerManager.php
@@ -11,9 +11,9 @@ class FilesCheckerManager {
   public function setupBatches() {
     \Drupal::state()->set('file_checker.file_checker_status',1);
     $file_count = \Drupal::entityQuery('file')->count()->execute();
-    \Drupal::state()->set('file_checker.batch_total',$file_count);
     $batch_size=100;
     $batches=ceil($file_count/$batch_size);
+    \Drupal::state()->set('file_checker.batch_total',$batches);
 
     for ($batch = 1; $batch <= $batches; $batch++) {
       $batchStart = 1 + (($batch-1)*($batch_size));
