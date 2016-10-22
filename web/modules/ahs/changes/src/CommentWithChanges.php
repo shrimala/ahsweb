49e92074ce5ff89c30e7ff1c6a543589b88bded7
Jonathan Shaw <jonathanjfshaw@gmail.com>
2016-10-22 11:59:30 +0100

    Added changes module

---
+++ web/modules/ahs/changes/src/CommentWithChanges.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Drupal\changes;
+
+use Drupal\comment\CommentInterface;
+use Drupal\comment\Entity\Comment;
+use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\Unicode;
+
+
+/**
+ * Class CommentWithChanges.
+ *
+ * @package Drupal\changes
+ */
+class CommentWithChanges {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManager definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+  public function add($comment, $entity, $oldRevisionId = NULL, $newRevisionId = NULL, $diffFieldName = NULL) {
+    // If the author of the change recently made another change or comment
+    // update that comment instead of making a new comment.
+    // This keeps the comments easier to read and reduces notification noise.
+    //expect array with keys mandatory keys
+    // 'comment_type', 'field_name'
+    // and optional keys 
+    // 'subject', 'comment_body' and 'uid'. 
+
+    // We omit to test comment_type is same current and last
+    // also will blow up if new fields added
+
+    //Default to current user as author
+    if (!isset($comment['uid'])) {
+      $comment['uid'] = \Drupal::currentUser()->id();
+    }
+
+    // Get last comment; can be null if no previous comments exist
+    $lastCommentValues = $entity->$comment['field_name']->getValue()[0];
+    $lastComment = Comment::load($lastCommentValues['cid']);
+    $lastSubject = NULL;
+    if (!empty($lastComment)) {
+      $lastSubject = $lastComment->getSubject();
+    }
+
+    $this->setDefaultSubjectIfNeeded($comment);
+
+    // Evaluate whether to update last comment or create new one
+    $hasLastSameAuthor = ($lastCommentValues['last_comment_uid'] === $comment['uid']);
+    $isLastWithinPeriod = ((REQUEST_TIME - $lastCommentValues['last_comment_timestamp']) < 30);
+    $isLastChangeRecord = ($lastSubject === 'Change record');
+    $isNewChangeRecord = ($comment['subject'] === 'Change record');
+    if ($lastComment && $hasLastSameAuthor && $isLastWithinPeriod && ($isLastChangeRecord || $isNewChangeRecord)) {
+      $this->updateLast($comment, $oldRevisionId, $newRevisionId, $diffFieldName, $entity, $lastComment);
+    }
+    else {
+      $this->createNew($comment, $oldRevisionId, $newRevisionId, $diffFieldName, $entity);
+    }
+  }
+
+  protected function updateLast($comment, $oldRevisionId, $newRevisionId, $diffFieldName, $entity, CommentInterface $lastComment) {
+    $entityType = $entity->getEntityTypeId();
+
+    // If there is a new revision of the host entity, update it on the comment
+    if (!empty($newRevisionId)) {
+      // If the last comment did not store changes, store the current revisions pair
+      if (empty($lastComment->$diffFieldName->entity_type)) {
+        $lastComment->$diffFieldName->setValue([
+          [
+            'left_rid' => $oldRevisionId,
+            'right_rid' => $newRevisionId,
+          ]
+        ]);
+      }
+      // If changes have already been stored, don't overwrite the older revision (left_rid)
+      elseif ($lastComment->$diffFieldName->entity_type === $entityType) {
+        $changes = $lastComment->$diffFieldName->getValue();
+        $changes[0]['right_rid'] = $newRevisionId;
+        $lastComment->$diffFieldName->setValue($changes);
+      }
+    }
+
+    // Overwrite the old subject only if it was a change record.
+    if ($lastComment->subject->value === 'Change Record') {
+      $lastComment->setSubject($comment['subject']);
+    }
+
+    // Set all fields for which data has been passed
+    // and there is no saved value
+    foreach ($comment as $name => $value) {
+      if (!isset($lastComment->$name->value)) {
+        if (is_array($value)) {
+          $lastComment->$name->setValue($value);
+        }
+        else {
+          $lastComment->$name->value = $value;
+        }
+      }
+    }
+
+      $lastComment->setCreatedTime(REQUEST_TIME);
+      $lastComment->save();
+  }
+
+  protected function createNew($comment, $oldRevisionId, $newRevisionId, $diffFieldName, $entity) {
+    // Create the comment
+    $commentDefaults = [
+      'langcode' => \Drupal::languageManager()->getCurrentLanguage()->getId(),
+      'entity_id' => $entity->id(),
+      'entity_type' => $entity->getEntityTypeId(),
+      'status' => 1,
+    ];
+
+    //Array merge: If the input arrays have the same string keys,
+    // then the later value for that key will overwrite the previous one.
+    $comment = array_merge($commentDefaults, $comment);
+
+    // If we have a revisionId, fill that field.
+    if (!empty($newRevisionId)) {
+      $comment[$diffFieldName] = [
+        'left_rid' => $oldRevisionId,
+        'right_rid' => $newRevisionId,
+      ];
+    }
+
+    $comment = Comment::create($comment);
+    $comment->setCreatedTime(REQUEST_TIME);
+    $comment->save();
+  }
+
+  protected function setDefaultSubjectIfNeeded(&$comment) {
+    // Extract subject from comment body.
+    if (!isset($comment['subject'])){
+      $comment['subject'] = '';
+    }
+    if ((trim($comment['subject']) == '')) {
+      if (isset($comment['comment_body'])) {
+        // The body may be in any format, so:
+        // 1) Filter it into HTML
+        // 2) Strip out all HTML tags
+        // 3) Convert entities back to plain-text.
+        $comment_text = $comment['comment_body']['value'];
+        $comment['subject'] = Unicode::truncate(trim(Html::decodeEntities(strip_tags($comment_text))), 29, TRUE, TRUE);
+      }
+      // Edge cases where the comment body is populated only by HTML tags will
+      // require a default subject.
+      if ($comment['subject'] == '') {
+        $comment['subject'] = t('(No subject)');
+      }
+    }
+  }
+}
---
+++ web/modules/ahs/changes/src/Plugin/diff/Layout/changesDiffLayout.php
@@ -0,0 +1,263 @@
+<?php
+
+namespace Drupal\changes\Plugin\diff\Layout;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\diff\DiffEntityComparison;
+use Drupal\diff\DiffEntityParser;
+use Drupal\diff\DiffLayoutBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use HtmlDiffAdvancedInterface;
+use Drupal\Core\PhpStorage\PhpStorageFactory;
+
+/**
+ * @DiffLayoutBuilder(
+ *   id = "changes",
+ *   label = @Translation("Condensed change record"),
+ * )
+ */
+class changesDiffLayout extends DiffLayoutBase {
+
+  /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * The diff entity comparison service.
+   *
+   * @var \Drupal\diff\DiffEntityComparison
+   */
+  protected $entityComparison;
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The html diff service.
+   *
+   * @var \HtmlDiffAdvancedInterface
+   */
+  protected $htmlDiff;
+
+  /**
+   * Constructs a SplitFieldsDiffLayout object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The configuration factory object.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\diff\DiffEntityParser $entity_parser
+   *   The entity parser.
+   * @param \Drupal\Core\DateTime\DateFormatter $date
+   *   The date service.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   * @param \Drupal\diff\DiffEntityComparison $entity_comparison
+   *   The diff entity comparison service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config, EntityTypeManagerInterface $entity_type_manager, DiffEntityParser $entity_parser, DateFormatter $date, RendererInterface $renderer, DiffEntityComparison $entity_comparison, RequestStack $request_stack, HtmlDiffAdvancedInterface $html_diff) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $config, $entity_type_manager, $entity_parser, $date);
+    $this->renderer = $renderer;
+    $this->entityComparison = $entity_comparison;
+    $this->requestStack = $request_stack;
+    $storage = PhpStorageFactory::get('html_purifier_serializer');
+    if (!$storage->exists('cache.php')) {
+      $storage->save('cache.php', 'dummy');
+    }
+    $html_diff->getConfig()->setPurifierCacheLocation(dirname($storage->getFullPath('cache.php')));
+    $this->htmlDiff = $html_diff;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+      $container->get('entity_type.manager'),
+      $container->get('diff.entity_parser'),
+      $container->get('date.formatter'),
+      $container->get('renderer'),
+      $container->get('diff.entity_comparison'),
+      $container->get('request_stack'),
+      $container->get('diff.html_diff')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(EntityInterface $left_revision, EntityInterface $right_revision, EntityInterface $entity) {
+    
+    $fields = $this->entityComparison->compareRevisions($left_revision, $right_revision);
+
+    // Build the diff rows for each field and append the field rows
+    // to the table rows.
+    $diff_rows = [];
+    foreach ($fields as $field) {
+      $field_row = [];
+      $field_row['label'] = [
+        'data' => !empty($field['#name']) ? $field['#name'] : '',
+        'class' => ['field-name'],
+      ];
+
+      // Strip HTML
+      $field_settings = $field['#settings'];
+      if (!empty($field_settings['settings']['markdown'])) {
+        $field['#data']['#left'] = $this->applyMarkdown($field_settings['settings']['markdown'], $field['#data']['#left']);
+        $field['#data']['#right'] = $this->applyMarkdown($field_settings['settings']['markdown'], $field['#data']['#right']);
+      }
+      // In case the settings are not loaded correctly use drupal_html_to_text
+      // to avoid any possible notices when a user clicks on markdown.
+      else {
+        $field['#data']['#left'] = $this->applyMarkdown('drupal_html_to_text', $field['#data']['#left']);
+        $field['#data']['#right'] = $this->applyMarkdown('drupal_html_to_text', $field['#data']['#right']);
+      }
+
+      // Process the array (split the strings into single line strings)
+      // and get line counts per field.
+      $this->entityComparison->processStateLine($field);
+
+      $field_diff_rows = $this->entityComparison->getRows(
+        $field['#data']['#left'],
+        $field['#data']['#right']
+      );
+
+      $final_diff = [];
+      $row_count = 0;
+      foreach ($field_diff_rows as $key => $value) {
+        $html_1 = isset($field_diff_rows[$key][1]['data']) ? $this->mb_trim(implode(' ', $field_diff_rows[$key][1]['data'])) : NULL;
+        $html_2 = isset($field_diff_rows[$key][3]['data']) ? $this->mb_trim(implode(' ', $field_diff_rows[$key][3]['data'])) : NULL;
+        // Ignore empty lines
+        $html_1 = ($html_1 == '&#160;' || $html_1 == '&nbsp;') ? '' : $html_1;
+        $html_2 = ($html_2 == '&#160;' || $html_1 == '&nbsp;') ? '' : $html_2;
+        if (!empty($html_1) || !empty($html_2)) {
+          $row_count++;
+          $this->htmlDiff->setOldHtml($html_1);
+          $this->htmlDiff->setNewHtml($html_2);
+          $this->htmlDiff->build();
+          $final_diff[] = [
+            '#markup' => '<p>' . $this->htmlDiff->getDifference() . '</p>',
+          ];
+        }
+      }
+
+      // Add the field row to the table only if there are changes to that field.
+      if (!empty($final_diff)) {
+        if ($row_count > 6) {
+          $final_diff = [
+            '#type' => 'details',
+            '#title' => t('Show changes'),
+            'details' => $final_diff,
+          ];
+        }
+        $field_row['diff'] = [
+          'data' => $final_diff,
+        ];
+        $diff_rows[] = $field_row;
+      }
+    }
+
+    $build['diff'] = [
+      '#type' => 'table',
+      '#rows' => $diff_rows,
+      '#weight' => 10,
+      '#empty' => $this->t('No visible changes'),
+      '#attributes' => [
+        'class' => ['diff'],
+      ],
+    ];
+
+    return $build;
+  }
+
+  /**
+   * Trim characters from either (or both) ends of a string in a way that is
+   * multibyte-friendly.
+   *
+   * Mostly, this behaves exactly like trim() would: for example supplying 'abc' as
+   * the charlist will trim all 'a', 'b' and 'c' chars from the string, with, of
+   * course, the added bonus that you can put unicode characters in the charlist.
+   *
+   * We are using a PCRE character-class to do the trimming in a unicode-aware
+   * way, so we must escape ^, \, - and ] which have special meanings here.
+   * As you would expect, a single \ in the charlist is interpretted as
+   * "trim backslashes" (and duly escaped into a double-\ ). Under most circumstances
+   * you can ignore this detail.
+   *
+   * As a bonus, however, we also allow PCRE special character-classes (such as '\s')
+   * because they can be extremely useful when dealing with UCS. '\pZ', for example,
+   * matches every 'separator' character defined in Unicode, including non-breaking
+   * and zero-width spaces.
+   *
+   * It doesn't make sense to have two or more of the same character in a character
+   * class, therefore we interpret a double \ in the character list to mean a
+   * single \ in the regex, allowing you to safely mix normal characters with PCRE
+   * special classes.
+   *
+   * *Be careful* when using this bonus feature, as PHP also interprets backslashes
+   * as escape characters before they are even seen by the regex. Therefore, to
+   * specify '\\s' in the regex (which will be converted to the special character
+   * class '\s' for trimming), you will usually have to put *4* backslashes in the
+   * PHP code - as you can see from the default value of $charlist.
+   *
+   * @param string
+   * @param charlist list of characters to remove from the ends of this string.
+   * @param boolean trim the left?
+   * @param boolean trim the right?
+   * @return String
+   */
+  protected function mb_trim($string, $charlist='\\\\s', $ltrim=true, $rtrim=true)
+  {
+    $both_ends = $ltrim && $rtrim;
+
+    $char_class_inner = preg_replace(
+      array( '/[\^\-\]\\\]/S', '/\\\{4}/S' ),
+      array( '\\\\\\0', '\\' ),
+      $charlist
+    );
+
+    $work_horse = '[' . $char_class_inner . ']+';
+    $ltrim && $left_pattern = '^' . $work_horse;
+    $rtrim && $right_pattern = $work_horse . '$';
+
+    if($both_ends)
+    {
+      $pattern_middle = $left_pattern . '|' . $right_pattern;
+    }
+    elseif($ltrim)
+    {
+      $pattern_middle = $left_pattern;
+    }
+    else
+    {
+      $pattern_middle = $right_pattern;
+    }
+
+    return preg_replace("/$pattern_middle/usSD", '', $string);
+    }
+
+}
