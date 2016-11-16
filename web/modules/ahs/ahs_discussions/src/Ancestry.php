602de80d1cdd9fe423c8c849b1a1a9bc30ffb1df
Jonathan Shaw <jonathanjfshaw@gmail.com>
2016-11-16 11:44:12 +0000

    Update child discussions ancestry_plain if title changes

--- web/modules/ahs/ahs_discussions/src/Ancestry.php
+++ web/modules/ahs/ahs_discussions/src/Ancestry.php
@@ -61,9 +61,56 @@ class Ancestry {
         }
         // Update the ancestry_plain field as well.
         $discussion->field_ancestry_plain = $this->makePlain($discussion->field_ancestry);
+        dpm("set ancestry plain on " . $discussion->label() . " as " . $discussion->field_ancestry_plain->value);
+        // No $discussion->save(); is needed as this is always called form a hook.
+        // Having one possibly creates an infinite loop.
       }
     }
     return $discussion;
+  }
+
+  /**
+   * Update ancestry and ancestry_plain field of any child that has this as its first parent.
+   *
+   * @var \Drupal\Core\Entity\EntityInterface $discussion
+   *    A discussion node that may have had its ancestry updating.
+   */
+  public function updateChildren(ContentEntityInterface $discussion) {
+    if (
+      $discussion->hasField('field_parents') &&
+      $discussion->hasField('field_ancestry') &&
+      $discussion->hasField('field_ancestry_plain')
+    ) {
+      // If own ancestry has changed, update children's ancestry.
+      $newAncestry = $discussion->field_ancestry->getValue();
+      $oldAncestry = empty($discussion->original) ? NULL : $discussion->original->field_ancestry->getValue();
+      $newTitle = $discussion->label();
+      $oldTitle = empty($discussion->original) ? NULL : $discussion->original->label();
+      if (($newAncestry !== $oldAncestry) || ($newTitle !== $oldTitle)) {
+        $children = $discussion->field_children->referencedEntities();
+        $childAncestry = $newAncestry;
+        $childAncestry[] = ["target_id" => $discussion->id()];
+        foreach ($children as $child) {
+          if (
+            $child->hasField('field_parents') &&
+            $child->hasField('field_ancestry') &&
+            $child->hasField('field_ancestry_plain')
+          ) {
+            // Only update children's ancestry if this is their first parent.
+            if ($child->field_parents->entity->id() === $discussion->id()) {
+              // Only update children's ancestry if child is not already included.
+              // Prevents crazy loops.
+              if (!$this->withinAncestry($child, $childAncestry)) {
+                $child->field_ancestry = $childAncestry;
+              }
+              $child->field_ancestry_plain = $this->makePlain($child->field_ancestry);
+              $child->save();
+              dpm("saved ancestry plain on " . $child->label() . " as " . $child->field_ancestry_plain->value);
+            }
+          }
+        }
+      }
+    }
   }
 
   /**
@@ -72,7 +119,7 @@ class Ancestry {
    * @var \Drupal\Core\Entity\EntityInterface $discussion
    *    A discussion node that may have had its ancestry updating.
    */
-  public function updateChildrensAncestry(ContentEntityInterface $discussion) {
+  protected function updateChildrensAncestry(ContentEntityInterface $discussion) {
     if (
       $discussion->hasField('field_parents') &&
       $discussion->hasField('field_ancestry') &&
@@ -95,6 +142,7 @@ class Ancestry {
                 $child->field_ancestry = $childAncestry;
                 $child->field_ancestry_plain = $this->makePlain($child->field_ancestry);
                 $child->save();
+                kint("saved ancestry plain on " . $child->label() . " as " . $child->field_ancestry_plain->value);
               }
             }
           }
