25205dcf5f9ecf10d498e40341d291ea73eea14d
Jonathan Shaw <jonathanjfshaw@gmail.com>
2017-03-04 17:33:53 +0000

    Working 8.3

--- scripts/ahs/localdeploy.sh
+++ scripts/ahs/localdeploy.sh
@@ -8,7 +8,7 @@ drush config-set "system.site" uuid "2748a6a9-0c67-43c6-aa47-71df4737a69f" --yes
 for module in `cat ../modules.common`; do drush --yes pm-enable $module; done
 for module in `cat ../modules.dev`; do drush --yes pm-enable $module; done
 drush cc drush
-drush --yes config-import
+drush --yes config-split-import
 
 drush --yes sql-sanitize --sanitize-email="no"
 drush entup --yes
