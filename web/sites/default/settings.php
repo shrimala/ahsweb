19f6f1a606ffb6ceccd386c6e0d684273ff7cb59
jonathanjfshaw <jonathanjfshaw@gmail.com>
2017-07-26 13:49:33 +0100

    Manually update files from platform example (#139)
    * Manually update files from platform example
    
    * Restore cron key
    
    * Restore mysql
    
    * Use app:http route

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -13,7 +13,11 @@
 $databases = [];
 $config_directories = [];
 $settings['update_free_access'] = FALSE;
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+$settings['file_scan_ignore_directories'] = [
+  'node_modules',
+  'bower_components',
+];
 
 // Install with the 'standard' profile for this example.
 //
@@ -32,14 +36,12 @@ $settings['install_profile'] = 'minimal';
 
 // Set up a config sync directory.
 //
-// This is defined inside the read-only "config" directory. This works well,
-// however it requires a patch from issue https://www.drupal.org/node/2607352
-// to fix the requirements check and the installer.
+// This is defined inside the read-only "config" directory.
 $config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
 
 // Automatic Platform.sh settings.
-if (file_exists(__DIR__ . '/settings.platformsh.php')) {
-  include __DIR__ . '/settings.platformsh.php';
+if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
+  include $app_root . '/' . $site_path . '/settings.platformsh.php';
 }
 
 if (isset($_ENV["PLATFORM_ROUTES"])) {
@@ -114,6 +116,6 @@ $config['config_split.config_split.development']['status'] = FALSE;
 $config['config_split.config_split.production']['status'] = TRUE;
 
 // Local settings. These come last so that they can override anything.
-if (file_exists(__DIR__ . '/settings.local.php')) {
-  include __DIR__ . '/settings.local.php';
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
 }
